11ecd0dc4c3338a8c652403a3f56d5e8114e425a
Ilya Batozskiy <ilya.batozskiy@raccoongang.com>
2015-11-27 08:17:52 +0200

    Fixes based on the comments on GitHub

--- mysite/ct/tests/integrate.py
+++ mysite/ct/tests/integrate.py
@@ -5,10 +5,8 @@ Tests branching behaviour of core app.
 """
 import time
 import requests
-from django.middleware.csrf import _get_new_csrf_key
-
-
 from mock import patch, Mock
+
 from django.test import TestCase
 from django.contrib.auth.models import User
 
@@ -451,8 +449,7 @@ class SMTPerrorTest(TestCase):
     @patch('psa.mail.send_mail')
     def smtp_error(self, mocked):
         mocked.side_effect = smtplib.SMTPServerDisconnected
-        post_data = {'csrfmiddlewaretoken': _get_new_csrf_key(),
-                     'email':'some@mail.com'}
+        post_data = {'email':'some@mail.com'}
         response = requests.post('/complete/email/?next=/ct/', data=post_data)
         content = response.content
         self.assertEqual(content.message, 'Something goes wrong with email sending. Please try again later.')
