907324498957b4a7d63cb57cdb7bca5a127a6f8e
Maksim Sokolskiy <misokolsky@gmail.com>
2015-12-16 18:18:47 +0200

    feat(ui): add 'ui/hack/' url as an entry point, move static to ui/static

--- mysite/ui/urls.py
+++ mysite/ui/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 
 from ui.views import CourseUnitsVew
 
 
 urlpatterns = patterns(
     '',
+    url(r'hack/$', TemplateView.as_view(template_name='ui/sidebar.html'), name='entry_point'),
     url(r'^api/courses/(?P<course_id>\d+)/units/$', CourseUnitsVew.as_view({'get': 'list'}), name='units_list'),
 )
