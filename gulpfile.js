bf0bdd992814e67a94dd9e62c493e447cec70750
Dmitry <derdmitry@gmail.com>
2016-01-19 17:24:08 +0200

    usage rjs for build

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,4 @@
-var watch = require('gulp-watch');
-
-function Logging(){
+function Logging() {
   var stream = through.obj(function (file, enc, callback) {
     console.log("stream:");
     console.log(file, enc, callback);
@@ -12,25 +10,24 @@ function Logging(){
 }
 
 var gulp = require('gulp'),
-    stylus = require('gulp-stylus'), 
-    csso = require('gulp-csso'), 
-    uglify = require('gulp-uglify'), 
-    concat = require('gulp-concat'),
-    minifyCSS = require('gulp-minify-css'),
-    autoprefixer = require('gulp-autoprefixer'),
-    order = require("gulp-order"),
-    rjs = require('gulp-requirejs');
+  stylus = require('gulp-stylus'),
+  csso = require('gulp-csso'),
+  uglify = require('gulp-uglify'),
+  concat = require('gulp-concat'),
+  minifyCSS = require('gulp-minify-css'),
+  autoprefixer = require('gulp-autoprefixer'),
+  order = require("gulp-order"),
+  rjs = require('gulp-requirejs');
 var through = require('through2');
 
 
-
 //  JS
-gulp.task('js-move', function() {
-   return gulp.src(['./mysite/assets/js/lib/*.js', './mysite/assets/js/src/*.js', '!./mysite/assets/js/lib/require.js'])
-       //.pipe(concat('main.min.js'))
-       .pipe(uglify())
-       .on('error', console.log)
-       .pipe(gulp.dest('./mysite/assets/static/js/'));
+gulp.task('js-move', function () {
+  return gulp.src(['./mysite/assets/js/lib/*.js', './mysite/assets/js/src/*.js', '!./mysite/assets/js/lib/require.js'])
+    //.pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .on('error', console.log)
+    .pipe(gulp.dest('./mysite/assets/static/js/'));
 
 });
 
@@ -39,105 +36,87 @@ gulp.task('copy_structure', function () {
     './mysite/assets/js/src/**/*',
     '!./mysite/assets/js/src/config.js',
     '!./mysite/assets/js/src/ct.js',
-    '!./mysite/assets/js/src/issue.js'
-    ])
+    '!./mysite/assets/js/src/issue.js'])
     .pipe(gulp.dest('./mysite/assets/static/js/'));
 });
 
-gulp.task('js', function() {
-   return gulp.src([
-       '!./mysite/assets/js/src/config.js',
-       '!./mysite/assets/js/src/issue.js',
-       '!./mysite/assets/js/lib/require.js',
-       '!./mysite/assets/js/lib/backbone.js',
-       './mysite/assets/js/lib/*.js',
-       './mysite/assets/js/src/*.js'])
-       .pipe(order([
-            "jquery-2.1.1.min.js",
-            "*.js",
+gulp.task('js', function () {
+  return gulp.src([
+    '!./mysite/assets/js/src/config.js',
+    '!./mysite/assets/js/src/issue.js',
+    '!./mysite/assets/js/lib/require.js',
+    '!./mysite/assets/js/lib/backbone.js',
+    './mysite/assets/js/lib/*.js',
+    './mysite/assets/js/src/*.js'])
+    .pipe(order([
+      "jquery-2.1.1.min.js",
+      "*.js",
 
-          ]))
-       .pipe(concat('main.min.js'))
-      // .pipe(uglify())
-       .on('error', console.log)
-       .pipe(gulp.dest('./mysite/assets/static/js/'));
-
-});
-
-gulp.task('js-require', function() {
-   return gulp.src(['./mysite/assets/js/lib/require.js', './mysite/assets/js/src/require.js'])
-       .pipe(uglify())
-       .on('error', console.log)
-       .pipe(gulp.dest('./mysite/assets/static/js/lib'));
+    ]))
+    .pipe(concat('main.min.js'))
+    // .pipe(uglify())
+    .on('error', console.log)
+    .pipe(gulp.dest('./mysite/assets/static/js/'));
 
 });
 
-gulp.task('require-config', function() {
-   return gulp.src(['./mysite/assets/js/src/config.js'])
-       .pipe(uglify())
-       .on('error', console.log)
-       .pipe(gulp.dest('./mysite/assets/static/js'));
+gulp.task('js-require', function () {
+  return gulp.src(['./mysite/assets/js/lib/require.js', './mysite/assets/js/src/require.js'])
+    .pipe(uglify())
+    .on('error', console.log)
+    .pipe(gulp.dest('./mysite/assets/static/js/lib'));
+
+});
+
+gulp.task('require-config', function () {
+  return gulp.src(['./mysite/assets/js/src/config.js'])
+    .pipe(uglify())
+    .on('error', console.log)
+    .pipe(gulp.dest('./mysite/assets/static/js'));
 
 });
 
 // CSS
-gulp.task('css', function() {
-    return gulp.src(['./mysite/assets/css/src/*.css', './mysite/assets/css/lib/*.css'])
-        //.pipe(minifyCSS())
-        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
-        .pipe(concat('main.css'))
-        .pipe(gulp.dest('./mysite/assets/static/css/'));
+gulp.task('css', function () {
+  return gulp.src(['./mysite/assets/css/src/*.css', './mysite/assets/css/lib/*.css'])
+    //.pipe(minifyCSS())
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest('./mysite/assets/static/css/'));
 });
 
 
-gulp.task('rbuild', function() {
-    rjs({
-        baseUrl: './mysite/assets/js/src/file.js',
-        out: './mysite/assets/static/js/',
-        shim: {
-            underscore: {
-                exports: '_'
-            },
-            backbone: {
-                deps: [
-                    'underscore',
-                    'jquery'
-                ],
-                exports: 'Backbone'
-            },
-            'backbone-relational': {
-                deps: [
-                    'backbone'
-                ],
-                exports: 'RelationalModel'
-            }
-        },
-    })
-        .pipe(gulp.dest('./delpoy/'));
-});
-
-gulp.task('watch-config', function () {
-    return gulp.src('./mysite/assets/js/src/config.js')
-        .pipe(watch('./mysite/assets/js/src/config.js'))
-        .pipe(gulp.dest('./mysite/assets/static/js'));
-});
-
-gulp.task('watch-structure', function () {
-    return gulp.src([
-    './mysite/assets/js/src/**/*',
-    '!./mysite/assets/js/src/config.js',
-    '!./mysite/assets/js/src/ct.js',
-    '!./mysite/assets/js/src/issue.js'
-    ])
-    .pipe(watch([
-    './mysite/assets/js/src/**/*',
-    '!./mysite/assets/js/src/config.js',
-    '!./mysite/assets/js/src/ct.js',
-    '!./mysite/assets/js/src/issue.js'
-    ]))
-    .pipe(gulp.dest('./mysite/assets/static/js'));
+gulp.task('rbuild', function () {
+  rjs({
+    name: "config",
+    baseUrl: './mysite/assets/js/src/',
+    out: 'main-app.js',
+    delimiter: '\t',
+    shim: {
+      underscore: {
+        exports: '_'
+      },
+      backbone: {
+        deps: [
+          'underscore',
+          'jquery'
+        ],
+        exports: 'Backbone'
+      },
+      backboneLocalstorage: {
+        deps: ['backbone',],
+        exports: 'Store'
+      }
+    },
+    paths: {
+      jquery: '../../../../node_modules/jquery/dist/jquery',
+      underscore: '../../../../node_modules/underscore/underscore',
+      backbone: '../../../../node_modules/backbone/backbone',
+      text: '../../../../node_modules/requirejs-text/text'
+    }
+  })
+    .pipe(uglify())
+    .pipe(gulp.dest('./mysite/assets/static/js/'));
 });
 
 gulp.task('default', ['copy_structure', 'js', 'js-require', 'require-config', 'css']);
-
-gulp.task('watch', ['watch-config', 'watch-structure']);
--- mysite/assets/js/src/config.js
+++ mysite/assets/js/src/config.js
@@ -1,44 +1,45 @@
 require.config({
-	// The shim config allows us to configure dependencies for
-	// scripts that do not call define() to register a module
-	shim: {
-		underscore: {
-			exports: '_'
-		},
-		backbone: {
-			deps: [
-				'underscore',
-				'jquery'
-			],
-			exports: 'Backbone'
-		},
-		backboneLocalstorage: {
-			deps: ['backbone'],
-			exports: 'Store'
-		}
-	},
-	paths: {
-		jquery: '../jquery/jquery',
-		underscore:'../underscore/underscore',
-		backbone: '../backbone/backbone',
-		text: '../text/text'
-	}
+  // The shim config allows us to configure dependencies for
+  // scripts that do not call define() to register a module
+  shim: {
+    underscore: {
+      exports: '_'
+    },
+    backbone: {
+      deps: [
+        'underscore',
+        'jquery'
+      ],
+      exports: 'Backbone'
+    },
+    backboneLocalstorage: {
+      deps: ['backbone'],
+      exports: 'Store'
+    }
+  },
+  paths: {
+    jquery: '../jquery/jquery',
+    underscore: '../underscore/underscore',
+    backbone: '../backbone/backbone',
+    text: '../text/text'
+  }
 });
 
 require([
-	'backbone',
-	'views/main_tab_view',
-	'utils/utils',
+  'backbone',
+  'views/main_tab_view',
+  'utils/utils',
 ], function (Backbone, AppView, utils) {
-    // Main access point for our app
-    var csrftoken = utils.getCookie('csrftoken');
-    var oldSync = Backbone.sync;
-    Backbone.sync = function(method, model, options){
-        options.beforeSend = function(xhr){
-            xhr.setRequestHeader('X-CSRFToken', csrftoken);
-        };
-        return oldSync(method, model, options);
+  // Main access point for our app
+  var csrftoken = utils.getCookie('csrftoken');
+  var oldSync = Backbone.sync;
+  Backbone.sync = function (method, model, options) {
+    options.beforeSend = function (xhr) {
+      xhr.setRequestHeader('X-CSRFToken', csrftoken);
     };
+    return oldSync(method, model, options);
+  };
 
-    new AppView({el:$('#lesson_issues')});
+  new AppView({el: $('#lesson_issues')});
+  console.log("Work");
 });
