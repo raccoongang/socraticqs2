dde8fb7bc5c8ff9af50f278f8f6f0757d229b57a
Ilya Batozskiy <ilya.batozskiy@raccoongang.com>
2016-01-21 15:15:06 +0200

    Fix router for lessons

--- mysite/assets/js/src/routers/router.js
+++ mysite/assets/js/src/routers/router.js
@@ -9,6 +9,9 @@ function ($, Backbone, Issues) {
 		routes: {
 			'ui/hack/lesson/:number(/)': 'getIssues',
 			'ui/hack/lesson/:number/:is_open(/)': 'is_open',
+            'ct/teach/courses/:course_id/units/:unit_id/concepts/:concept_id(/)': 'Issues',
+            'ct/teach/courses/:course_id/units/:unit_id/concepts/:concept_id/issues': 'openIssues'
+
 		},
 
 		is_open: function (number, is_open) {
@@ -19,6 +22,14 @@ function ($, Backbone, Issues) {
 
 		getIssues: function(number){
             Backbone.trigger('unit_lesson',{unit_lesson: number});
+        },
+
+        Issues: function(course_id, unit_id, concept_id){
+            Backbone.trigger('unit_lesson',{unit_lesson: concept_id});
+        },
+
+        openIssues: function(course_id, unit_id, concept_id){
+            Backbone.trigger('unit_lesson',{unit_lesson: concept_id});
         },
 
 	});
