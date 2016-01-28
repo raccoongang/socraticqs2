12db559f8f98fb2306a207ffa4c96c8e71953256
Maksim Sokolskiy <misokolsky@gmail.com>
2016-01-28 12:24:22 +0200

    ref(assets:search): update search results on each 3th letter

--- mysite/assets/js/src/views/search_view.js
+++ mysite/assets/js/src/views/search_view.js
@@ -33,7 +33,7 @@ define([
                     $('#searchText').val(param.text);
                 }
                 this.search = (param.text) ? param.text : $('#searchText').val();
-                if (this.search.length >= 3) {
+                if (this.search.length % 3 == 0) {
                     SearchCollection.fetch({data: {text:this.search}, reset: true});
                 }
             },
